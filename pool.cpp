a8e4d8b7cf07551c25d165bd297ac1473654b4a0
xzdd@xzdds-MBP <56295612@qq.com>
2020-06-27 19:37:56 +0800

    add test.cpp, add comments, split test code and split pool.hpp

--- pool.cpp
+++ pool.cpp
@@ -1,20 +1,78 @@
 #include "pool.hpp"
+using namespace mem;
 
-int main()
+PoolMemory::PoolMemory(const std::size_t block_sz_bytes, const std::size_t num_blocks)
+    : m_pool_sz_bytes(num_blocks * block_sz_bytes),
+      m_block_sz_bytes(block_sz_bytes),
+      m_free_num_blocks(num_blocks),
+      m_total_num_blocks(num_blocks)
+
 {
-    int num_elements = 10;
-    mem::PoolMemory pool(sizeof(double), num_elements);
-    std::vector<double *> ptrs;
-    for (auto i = 0; i < num_elements; i++) {
-        ptrs.push_back(static_cast<double *>(pool.get()));
-        std::cout << "Getting element: " << ptrs.back() << " from the memory pool" << std::endl;
-        std::cout << "Currently we have " << pool.free_list_size() << " free element space" << std::endl;
-        std::cout << "Is the memory pool empty? " << (pool.empty() ? "Yes." : "No.") << std::endl;
+    /** We would want the size of the of the block to be bigger than a pointer */
+    assert(sizeof(void *) <= m_block_sz_bytes);
+    m_pmemory = new std::byte[m_pool_sz_bytes];      // using byte as memory pool base type
+    m_phead = reinterpret_cast<void **>(m_pmemory);  // treat list pointer as a pointer to pointer
+
+    /** We're using uintptr_t to perform arithmetic operations with confidence
+         * We're not using void * since, well, it's forbidden to perform arithmetic operations on a void *
+         */
+    std::uintptr_t start_addr = reinterpret_cast<std::uintptr_t>(m_pmemory);  // where the whole chunk memory begins
+    std::uintptr_t end_addr = start_addr + m_pool_sz_bytes;                   // where the chunk memory ends
+
+    /** We use the same space as the actual block to be stored here to store the free list pointers */
+    // construct the linked list from raw memory
+    for (auto i = 0; i < m_total_num_blocks; i++) {
+        std::uintptr_t curr_addr = start_addr + i * m_block_sz_bytes;  // current block's address
+        std::uintptr_t next_addr = curr_addr + m_block_sz_bytes;       // next block's address
+        void **curr_mem = reinterpret_cast<void **>(curr_addr);        // a pointer, same value as curr_addr to support modification
+        if (next_addr >= end_addr) {
+            *curr_mem = nullptr;
+        } else {
+            *curr_mem = reinterpret_cast<void *>(next_addr);
+        }
     }
-    pool.get();
-    for (auto &ptr : ptrs) {
-        std::cout << "Returning memory: " << ptr << " to the memory pool" << std::endl;
-        pool.free(ptr);
-        std::cout << "Currently we have " << pool.free_list_size() << " free element space" << std::endl;
+}
+
+void *PoolMemory::get()
+{
+    if (m_pmemory == nullptr) {  // This should not happen
+        std::cerr << "ERROR " << __FUNCTION__ << ": No memory was allocated to this pool" << std::endl;
+        return nullptr;
+    }
+    if (m_phead != nullptr) {
+        m_free_num_blocks--;  // decrement the number of free blocks
+
+        void *pblock = static_cast<void *>(m_phead);  // get current free list value
+        m_phead = static_cast<void **>(*m_phead);     // update free list head
+
+        return pblock;
+    } else {  // out of memory blocks (for an block with size m_block_sz_bytes)
+        std::cerr << "ERROR " << __FUNCTION__ << ": out of memory blocks" << std::endl;
+        return nullptr;  // if you get a nullptr from a memory pool, it's time to allocate a new one
+    }
+}
+
+void PoolMemory::free(void *pblock)
+{
+    if (pblock == nullptr) {
+        // do nothing if we're freeing a nullptr
+        // although this situation is declared undefined in C++ Standard
+        return;
+    }
+
+    if (m_pmemory == nullptr) {  // this should not happen
+        std::cerr << "ERROR " << __FUNCTION__ << ": No memory was allocated to this pool" << std::endl;
+        return;
+    }
+
+    m_free_num_blocks++;  // increment the number of blocks
+
+    if (m_phead == nullptr) {  // the free list is full (we can also check this by validating size)
+        m_phead = static_cast<void **>(pblock);
+        *m_phead = nullptr;
+    } else {
+        void *ppreturned_block = static_cast<void *>(m_phead);  // temporaryly store the current head as nex block
+        m_phead = static_cast<void **>(pblock);
+        *m_phead = ppreturned_block;
     }
 }
---
+++ test.cpp
@@ -0,0 +1,21 @@
+#include <vector>
+#include "pool.hpp"
+
+int main()
+{
+    int num_elements = 10;
+    mem::PoolMemory pool(sizeof(double), num_elements);
+    std::vector<double *> ptrs;
+    for (auto i = 0; i < num_elements; i++) {
+        ptrs.push_back(static_cast<double *>(pool.get()));
+        std::cout << "Getting element: " << ptrs.back() << " from the memory pool" << std::endl;
+        std::cout << "Currently we have " << pool.free_count() << " free element space" << std::endl;
+        std::cout << "Is the memory pool empty? " << (pool.empty() ? "Yes." : "No.") << std::endl;
+    }
+    pool.get();
+    for (auto &ptr : ptrs) {
+        std::cout << "Returning memory: " << ptr << " to the memory pool" << std::endl;
+        pool.free(ptr);
+        std::cout << "Currently we have " << pool.free_count() << " free element space" << std::endl;
+    }
+}
